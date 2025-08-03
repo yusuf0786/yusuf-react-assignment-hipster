3aa4e5d7bed35811a4689fb8fa7ff9b45415db55
yusuf0786 <yusssuf0786@gmail.com>
2025-08-03 16:35:56 +0530

    tailwind css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import ContactPage from "./pages/ContactPage"
 import "./App.css"
+import "../styles/globals.css"
 
 function App() {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust this path to your actual code location
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
